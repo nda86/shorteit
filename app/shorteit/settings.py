8829f38a198c585ad4a1641489dd088db47ea5a0
nda86 <nda86@mail.ru>
2020-10-10 19:47:04 +0800

    add STATIC_ROOT to settings

--- app/shorteit/settings.py
+++ app/shorteit/settings.py
@@ -111,6 +111,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 
 # сичтываем и загружаем настройки для логирования
