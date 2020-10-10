2bf02651311d4f6a68690c9dc2a935e519595478
nda86 <nda86@mail.ru>
2020-10-10 19:42:25 +0800

    add celery to supervisor

--- app/shorteit/celery.py
+++ app/shorteit/celery.py
@@ -4,6 +4,6 @@ from celery import Celery
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'shorteit.settings')
 
-app = Celery('shortit')
+app = Celery('shorteit')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
